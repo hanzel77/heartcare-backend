ffb9aeb67093bf86908837f29023eab86fa8515e
hanzel77 <hhanzel0810@gmail.com>
2024-12-09 13:35:30 +0700

    Changing Env

--- app.py
+++ app.py
@@ -375,4 +375,4 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=os.getenv("PORT", 5000))
+        app.run(host='0.0.0.0', port=os.getenv("PORT") | 3000)
