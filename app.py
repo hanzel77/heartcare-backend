ad5a3dd7a4a58f05408b7553dfc7af4caf60f627
hanzel77 <hhanzel0810@gmail.com>
2024-12-09 12:19:43 +0700

    Initial Commit

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import pickle
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:fWjPJdnLkrBjLzRzfQhHhLUlvaIsDHjv@mysql.railway.internal:3306/heartcare'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("MYSQL_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
